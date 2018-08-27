a2b2f1bd2c2229524eb2119479d7cc652830a76b
Mark Wecke <wecke@neoskop.de>
2018-08-27 18:41:53 +0200

    add customizable header

--- frontend/apps/huectl/src/app/header/header.component.ts
+++ frontend/apps/huectl/src/app/header/header.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, TemplateRef, ViewEncapsulation } from '@angular/core';
 import { AppState } from '../+state/app.state';
 import { Store } from '@ngrx/store';
 import { OpenSetupDialog } from '../+state/setup.actions';
 import { OpenConfigDialog } from '../+state/config.actions';
 
 @Component({
-  selector: 'hc-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass'],
-  encapsulation: ViewEncapsulation.None
+    selector     : 'hc-header',
+    templateUrl  : './header.component.html',
+    styleUrls    : [ './header.component.sass' ],
+    encapsulation: ViewEncapsulation.None
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor(protected readonly store : Store<AppState>) { }
-
-  ngOnInit() {
-  }
-
-  openSetup() {
-      this.store.dispatch(new OpenSetupDialog());
-  }
-  
-  openConfig() {
-      this.store.dispatch(new OpenConfigDialog());
-  }
+    @ContentChild('title', { read: TemplateRef })
+    title : TemplateRef<any>;
+    
+    constructor(protected readonly store : Store<AppState>) {
+    }
+    
+    openSetup() {
+        this.store.dispatch(new OpenSetupDialog());
+    }
+    
+    openConfig() {
+        this.store.dispatch(new OpenConfigDialog());
+    }
 }
